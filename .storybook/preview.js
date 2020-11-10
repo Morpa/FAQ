c0dbbac642316e3e6de520825f124ed78c6d5fc3
Andre Morpa <andremorpa@gmail.com>
2020-11-10 11:33:40 +0000

    Configurate storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,30 @@
-import GlobalStyles from '../src/styles/global'
+import { ThemeProvider } from 'styled-components'
+import GlobalStyles from 'styles/global'
+import theme from 'styles/theme'
+
+export const parameters = {
+  backgrounds: {
+    default: 'won-light',
+    values: [
+      {
+        name: 'won-light',
+        value: theme.colors.white
+      },
+      {
+        name: 'won-dark',
+        value: theme.colors.mainBg
+      }
+    ]
+  }
+}
 
 export const decorators = [
   (Story) => (
     <>
-      <GlobalStyles />
-      <Story />
+      <ThemeProvider theme={theme}>
+        <GlobalStyles removeBg />
+        <Story />
+      </ThemeProvider>
     </>
   )
 ]
